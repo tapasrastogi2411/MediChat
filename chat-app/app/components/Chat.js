2924faccb6946e70660fc197e60b40b798970bbf
Tapas Rastogi <tapas.rastogi@mail.utoronto.ca>
2024-04-17 19:54:24 -0400

    style: Made UI a lot better and easier on the eyes

--- chat-app/app/components/Chat.js
+++ chat-app/app/components/Chat.js
@@ -1,13 +1,13 @@
 // components/Chat.js
 'use client'
-// components/Chat.js
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Message from './Message';
 
 function Chat() {
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState('');
   const [sender, setSender] = useState('doctor'); // New state for sender
+  const messagesEndRef = useRef(null); // New ref for the last message
 
   const handleSend = () => {
     const message = {
@@ -24,18 +24,39 @@ function Chat() {
     setSender(sender === 'doctor' ? 'patient' : 'doctor');
   };
 
+  useEffect(() => {
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [messages]);
+
   return (
-    <div>
-      {messages.map(message => (
-        <Message key={message.id} message={message} />
-      ))}
-      <input
-        className="text-black"
-        value={newMessage}
-        onChange={e => setNewMessage(e.target.value)}
-      />
-      <button onClick={handleSend}>Send</button>
-      <button onClick={toggleSender}>Switch to {sender === 'doctor' ? 'patient' : 'doctor'}</button> {/* New button to switch sender */}
+    <div className="flex flex-col h-screen bg-gray-100">
+      <div className="overflow-auto">
+        {messages.map(message => (
+          <Message key={message.id} message={message} />
+        ))}
+        <div ref={messagesEndRef} /> {/* New div for the ref */}
+      </div>
+      <div className="flex items-center p-4 bg-white border-t border-gray-200">
+        <input
+          className="flex-1 px-4 py-2 text-gray-700 bg-gray-200 rounded-lg focus:outline-none"
+          value={newMessage}
+          onChange={e => setNewMessage(e.target.value)}
+        />
+        <button 
+          className="ml-4 px-4 py-2 text-white bg-blue-500 rounded-lg focus:outline-none"
+          onClick={handleSend}
+        >
+          Send
+        </button>
+        <button 
+          className="ml-4 px-4 py-2 text-white bg-green-500 rounded-lg focus:outline-none"
+          onClick={toggleSender}
+        >
+          Switch to {sender === 'doctor' ? 'patient' : 'doctor'}
+        </button>
+      </div>
     </div>
   );
 }
--- chat-app/app/components/Message.js
+++ chat-app/app/components/Message.js
@@ -1,7 +1,9 @@
+// components/Message.js
 function Message({ message }) {
     return (
-      <div>
-        <strong>{message.sender}</strong>: {message.text}
+      <div className={`m-4 p-4 rounded-lg text-black ${message.sender === 'doctor' ? 'bg-blue-200' : 'bg-green-200'}`}>
+        <p className="font-bold">{message.sender}</p>
+        <p>{message.text}</p>
       </div>
     );
   }
